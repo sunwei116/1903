f731714fdbc1f1bde3fa1127feb39142d655f6f9
sunwei116 <583337529@qq.com>
2019-12-18 18:22:04 +0800

    sunwei

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -13,23 +13,8 @@ class IndexController extends Controller
     //前台登录
     public function login(Request $request)
     {
-<<<<<<< Updated upstream
-
-         $re=$request->all();
-        $info=User::where(['user_name'=>$re['user_name']])->first();
-        if($info==null)
-        {
-            $arr=[
-                'error'=>2,
-                'msg'=>'用户不存在',
-                'data'=>null
-            ];
-=======
-        $token = '4389dd2f5aec6294dd5a20d9f8654208aed98712';
-        dd(User::checkToken($token));
         if(empty($_POST['user_name']) || empty($_POST['user_pwd'])) {
             echo json_encode(['code' => 2, 'msg' => '请输入账号和密码', 'data' => null]);exit;
->>>>>>> Stashed changes
 
         }
         $user_name=$_POST['user_name'];
