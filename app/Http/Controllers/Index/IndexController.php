479c321a1a154d92c828e5515461709344869bfa
sunwei116 <583337529@qq.com>
2019-12-24 20:31:33 +0800

    sunwei

--- app/Http/Controllers/Index/IndexController.php
+++ app/Http/Controllers/Index/IndexController.php
@@ -2,125 +2,109 @@
 
 namespace App\Http\Controllers\Index;
 
-use App\Model\Images;
+use App\Http\Controllers\Controller;
+use App\IndexModel\Car;
+use App\IndexModel\User;
+use App\Model\Attr;
+use App\Model\Category;
+use App\Model\Goods;
 use DemeterChain\C;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\IndexModel\User;
-use App\Model\Goods;
-use App\Model\Category;
-use App\Model\Attr;
-use App\IndexModel\Car;
 
-class IndexController extends Controller
-{
+class IndexController extends Controller {
 
-    //前台登录
-    public function login(Request $request)
-    {
-        User::header();
-        if(empty($_POST['user_name']) || empty($_POST['user_pwd'])) {
-            echo json_encode(['code' => 2, 'msg' => '请输入账号和密码', 'data' => null]);exit;
+	//前台登录
+	public function login(Request $request) {
+		User::header();
+		if (empty($_POST['user_name']) || empty($_POST['user_pwd'])) {
+			echo json_encode(['code' => 2, 'msg' => '请输入账号和密码', 'data' => null]);exit;
 
-        }
+		}
 
-        $user_name=$_POST['user_name'];
-        $user_pwd=$_POST['user_pwd'];
-        return User::login($user_name,$user_pwd);
-    }
+		$user_name = $_POST['user_name'];
+		$user_pwd = $_POST['user_pwd'];
+		return User::login($user_name, $user_pwd);
+	}
 
-    /**
-     * @centent 注册
-     */
-    public function register()
-    {
-        User::header();
-        $user_name = isset($_POST['user_name']) ? $_POST['user_name'] : '';
-        $user_pwd = isset($_POST['user_pwd']) ? $_POST['user_pwd'] : '';
-        $user_phone = isset($_POST['user_phone']) ? $_POST['user_phone'] : '';
-        return User::register($user_name, $user_pwd, $user_phone);
-    }
+	/**
+	 * @centent 注册
+	 */
+	public function register() {
+		User::header();
+		$user_name = isset($_POST['user_name']) ? $_POST['user_name'] : '';
+		$user_pwd = isset($_POST['user_pwd']) ? $_POST['user_pwd'] : '';
+		$user_phone = isset($_POST['user_phone']) ? $_POST['user_phone'] : '';
+		return User::register($user_name, $user_pwd, $user_phone);
+	}
 
+	/**
+	 * @centent 获取所有商品
+	 */
+	public function get_goods_all() {
+		User::header();
+		$data = Goods::get_Goods_all();
+		echo json_encode(['code' => 1, 'msg' => '请求成功', 'data' => $data]);
+	}
 
-    /**
-     * @centent 获取所有商品
-     */
-    public function get_goods_all()
-    {
-        User::header();
-        $data = Goods::get_Goods_all();
-       echo json_encode(['code'=>1,'msg'=>'请求成功','data'=>$data]);
-    }
+	/**
+	 * @centent 商品详情
+	 */
+	public function goods_desc() {
+		User::header();
+		//接受商品id
+		$goods_id = \request()->input('goods_id');
+		if (!isset($goods_id)) {
+			Goods::json_error(2, '请选择商品');
+		}
+		$data = Goods::goods_desc($goods_id);
+		Goods::json_success(1, '操作成功', $data);
+	}
 
-    /**
-     * @centent 商品详情
-     */
-    public function goods_desc()
-    {
-        User::header();
-        //接受商品id
-        $goods_id = \request()->input('goods_id');
-        if (!isset($goods_id)){
-            Goods::json_error(2,'请选择商品');
-        }
-        $data = Goods::goods_desc($goods_id);
-        Goods::json_success(1,'操作成功',$data);
-    }
+	public function get_goods_attr() {
+		User::header();
+		$goods_id = \request()->input('goods_id');
+		if (empty($goods_id)) {
+			Goods::json_error(2, '请选择商品');
+		}
+		$data = Attr::get_goods_attr($goods_id);
+		Goods::json_success(1, '操作成功', $data);
+	}
 
-    public function get_goods_attr()
-    {
-        User::header();
-        $goods_id = \request()->input('goods_id');
-        if (empty($goods_id)) {
-            Goods::json_error(2,'请选择商品');
-        }
-        $data = Attr::get_goods_attr($goods_id);
-        Goods::json_success(1,'操作成功',$data);
-    }
+	/**
+	 * @centent 首页分类展示
+	 */
+	public function category() {
+		User::header();
+		$cateInfo = Category::get();
+		Goods::json_success(1, '操作成功', $cateInfo);
 
-    /**
-     * @centent 首页分类展示
-     */
-    public  function category()
-    {
-        User::header();
-        $cateInfo=Category::get();
-        Goods::json_success(1,'操作成功',$cateInfo);
+	}
 
-    }
+	public function category_goods_list(Request $request) {
+		User::header();
+		$id = $request->get('category_id');
 
-    public function category_goods_list(Request $request)
-    {
-        User::header();
-         $id=$request->get('category_id');
-
-         $data=Goods::where('category_id',$id)->get();
-         Goods::json_success('1','调用接口成功',$data);
-    }
-    //加入购物车
-    public function add_car()
-    {
-        User::header();
-        $goods_id = $_POST['goods_id'];
-        $goods_name = $_POST['goods_name'];
-        $market_price = $_POST['market_price'];
+		$data = Goods::where('category_id', $id)->get();
+		Goods::json_success('1', '调用接口成功', $data);
+	}
+	//加入购物车
+	public function add_car() {
+		User::header();
+		$goods_id = $_POST['goods_id'];
+		$goods_name = $_POST['goods_name'];
+		$market_price = $_POST['market_price'];
 //        $goods_guige = $_POST['goods_guige'];
-        $token = $_POST['token'];
-        if (empty($token)) {
-            Goods::json_error(2,'token不存在，请先存在');
-        }
+		$token = $_POST['token'];
+		if (empty($token)) {
+			Goods::json_error(2, 'token不存在，请先存在');
+		}
 
-        Car::insert([
-            'goods_id'=>$goods_id,
-            'goods_name'=>$goods_name,
-            'market_price'=>$market_price
-        ]);
+		Car::insert([
+			'goods_id' => $goods_id,
+			'goods_name' => $goods_name,
+			'market_price' => $market_price,
+		]);
 
-
-
-
-
-
-
+	}
 
 }
--- app/IndexModel/User.php
+++ app/IndexModel/User.php
@@ -4,114 +4,106 @@ namespace App\IndexModel;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
-{
-    protected $table = 'user';
-    //是否更新时间戳 false
-    public $timestamps=false;
-    //主键
-    protected $primaryKey="user_id";
+class User extends Model {
+	protected $table = 'user';
+	//是否更新时间戳 false
+	public $timestamps = false;
+	//主键
+	protected $primaryKey = "user_id";
 
-    //登录
-    public static function login($user_name, $user_pwd)
-    {
-       //根据用户名密码查询
-        $data = self::where(['user_name'=>$user_name])->where(['user_pwd'=>$user_pwd])->first();
+	//登录
+	public static function login($user_name, $user_pwd) {
+		//根据用户名密码查询
+		$data = self::where(['user_name' => $user_name])->where(['user_pwd' => $user_pwd])->first();
 //        //查不到此用户
-        if (empty($data)) {
-            echo json_encode(['code' => 2, 'msg' => '账号或密码输入错误', 'data' => $data]);
-            exit;
-        }
-        $user_id = $data['user_id'];
+		if (empty($data)) {
+			echo json_encode(['code' => 2, 'msg' => '账号或密码输入错误', 'data' => $data]);
+			exit;
+		}
+		$user_id = $data['user_id'];
 //        生成过期时间
-        $time_out = time()+3600;
-        $token = self::setToken();
-        //更新touken
-        $user = self::find($user_id);
-        $user->time_out = $time_out;
-        $user->token = $token;
-        $res = $user->save();
-        if ($res) {
-            echo json_encode(['code' => 1, 'msg' => '登录成功', 'data' => ['token'=>$token]]);die;
-        }else{
-            echo json_encode(['code' => 2, 'msg' => '登录失败', 'data' => $data]);die;
-        }
-    }
-    //注册
-    public static function register($user_name, $user_pwd, $user_phone)
-    {
-        if (!empty($user_name) && !empty($user_pwd) && !empty($user_phone)) {
-            $data = self::where(['user_name'=>$user_name])->where('user_phone',$user_phone)->first();
-            if ($data) {
-                return json_encode(['msg' => '用户名或手机号已被注册', 'data' => null, 'code' => 2]);exit;
-            }else{
-                $res = self::insert([
-                    'user_name' => $user_name,
-                    'user_pwd' => $user_pwd,
-                    'user_phone' => $user_phone
-                ]);
-                if ($res) {
-                    return json_encode(['msg' => '注册成功', 'data' => $res, 'code' => 1]);exit;
-                } else {
-                    return json_encode(['msg' => '注册失败', 'data' => $res, 'code' => 2]);exit;
-                }
-            }
-        }
-    }
-    //生成token
-    public static function setToken()
-    {
-        $str = md5(uniqid(md5(microtime(true)),true));
-        $str = sha1($str);
-        return $str;
-    }
-    //查看token
-    public static function checkToken($token)
-    {
-        if (empty($token)) {
-            echo json_encode(['code' => 2, 'msg' => '请求失败', 'data' => null]);exit;
-        }
-        $tokenCheck = self::checkTokenFct($token);
-        if ($tokenCheck != 0001) {
-            echo json_encode(['code' => 2, 'msg' => 'token验证失败,请重新登录', 'data' => null]);exit;
-        }
-        return true;
+		$time_out = time() + 3600;
+		$token = self::setToken();
+		//更新touken
+		$user = self::find($user_id);
+		$user->time_out = $time_out;
+		$user->token = $token;
+		$res = $user->save();
+		if ($res) {
+			echo json_encode(['code' => 1, 'msg' => '登录成功', 'data' => ['token' => $token]]);die;
+		} else {
+			echo json_encode(['code' => 2, 'msg' => '登录失败', 'data' => $data]);die;
+		}
+	}
+	//注册
+	public static function register($user_name, $user_pwd, $user_phone) {
+		if (!empty($user_name) && !empty($user_pwd) && !empty($user_phone)) {
+			$data = self::where(['user_name' => $user_name])->where('user_phone', $user_phone)->first();
+			if ($data) {
+				return json_encode(['msg' => '用户名或手机号已被注册', 'data' => null, 'code' => 2]);exit;
+			} else {
+				$res = self::insert([
+					'user_name' => $user_name,
+					'user_pwd' => $user_pwd,
+					'user_phone' => $user_phone,
+				]);
+				if ($res) {
+					return json_encode(['msg' => '注册成功', 'data' => $res, 'code' => 1]);exit;
+				} else {
+					return json_encode(['msg' => '注册失败', 'data' => $res, 'code' => 2]);exit;
+				}
+			}
+		}
+	}
+	//生成token
+	public static function setToken() {
+		$str = md5(uniqid(md5(microtime(true)), true));
+		$str = sha1($str);
+		return $str;
+	}
+	//查看token
+	public static function checkToken($token) {
+		if (empty($token)) {
+			echo json_encode(['code' => 2, 'msg' => '请求失败', 'data' => null]);exit;
+		}
+		$tokenCheck = self::checkTokenFct($token);
+		if ($tokenCheck != 0001) {
+			echo json_encode(['code' => 2, 'msg' => 'token验证失败,请重新登录', 'data' => null]);exit;
+		}
+		return true;
 
-    }
-    //token验证
-    public static function checkTokenFct($token)
-    {
-        $res = self::where(['token'=>$token])->first();
-        if (!empty($res)) {
-            if ((time() - $res->time_out) > 0) {
-                echo json_encode(['code' => 2, 'msg' => 'token长时间未使用而过期，需重新登陆', 'data' => null]);exit;
-            }else{
-                $new_time_out = time()+3600;
-                $res->time_out = $new_time_out;
-                $res = $res->save();
-                if ($res) {
-                    return 0001;  //token验证成功，time_out刷新成功，可以获取接口信息
-                }else{
-                    return 0002;  //token错误验证失败
-                }
-            }
-        }
-        echo json_encode(['code' => 404, 'msg' => '请先登陆', 'data' => null]);exit;
+	}
+	//token验证
+	public static function checkTokenFct($token) {
+		$res = self::where(['token' => $token])->first();
+		if (!empty($res)) {
+			if ((time() - $res->time_out) > 0) {
+				echo json_encode(['code' => 2, 'msg' => '登录过期，需重新登陆', 'data' => null]);exit;
+			} else {
+				$new_time_out = time() + 3600;
+				$res->time_out = $new_time_out;
+				$res = $res->save();
+				if ($res) {
+					return 0001; //token验证成功，time_out刷新成功，可以获取接口信息
+				} else {
+					return 0002; //token错误验证失败
+				}
+			}
+		}
+		echo json_encode(['code' => 404, 'msg' => '请先登陆', 'data' => null]);exit;
 
-    }
+	}
 
-    public static function header()
-    {
-        header('Access-Control-Allow-Origin:*');
-        // 响应类型
-        header('Access-Control-Allow-Methods:*');
-        //请求头
-        header('Access-Control-Allow-Headers:*');
-        // 响应头设置
-        header('Access-Control-Allow-Credentials:false');
-        //数据类型
-        header('content-type:application:json;charset=utf8');
-    }
-
+	public static function header() {
+		header('Access-Control-Allow-Origin:*');
+		// 响应类型
+		header('Access-Control-Allow-Methods:*');
+		//请求头
+		header('Access-Control-Allow-Headers:*');
+		// 响应头设置
+		header('Access-Control-Allow-Credentials:false');
+		//数据类型
+		header('content-type:application:json;charset=utf8');
+	}
 
 }
